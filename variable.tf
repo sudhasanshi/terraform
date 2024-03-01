5df05d4f851fbdc95d95710a60495fefc4ca5051
sudha sanshi <35460293+sudhasanshi@users.noreply.github.com>
2024-03-01 19:37:39 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,32 @@
+
+variable "instance_ami" {
+  description = "instance ami"
+  type =  string
+  default = "ami-03bb6d83c60fc5f7c"
+}
+
+variable "instance_type" {
+  description = "instance type name"
+  type =  string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+    type = string
+    default = "user" 
+}
+
+variable "instance_name" {
+  type = string
+  default = "my-ec2"
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+variable "location" {
+  default = "ap-south-1"
+}
